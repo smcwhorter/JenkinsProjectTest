dc3a5075357970f6968f3663e73e55e075826f85
Steven McWhorter <smcwhorter@cardinalsolutions.com>
2012-11-05 11:53:27 -0500

    initial project files

---
+++ gen/R.java.d
@@ -0,0 +1,7 @@
+/Users/smcwhorter/Documents/Android Applications/JenkinsProjectTest/gen/com/cardinal/jenkinsproject/test/R.java \
+ : /Users/smcwhorter/Documents/Android Applications/JenkinsProjectTest/res/drawable-hdpi/ic_launcher.png \
+/Users/smcwhorter/Documents/Android Applications/JenkinsProjectTest/res/drawable-ldpi/ic_launcher.png \
+/Users/smcwhorter/Documents/Android Applications/JenkinsProjectTest/res/drawable-mdpi/ic_launcher.png \
+/Users/smcwhorter/Documents/Android Applications/JenkinsProjectTest/res/drawable-xhdpi/ic_launcher.png \
+/Users/smcwhorter/Documents/Android Applications/JenkinsProjectTest/res/values/strings.xml \
+/Users/smcwhorter/Documents/Android Applications/JenkinsProjectTest/bin/AndroidManifest.xml \
---
+++ src/com/cardinal/jenkinsproject/test/UtilsTest.java
@@ -0,0 +1,29 @@
+package com.cardinal.jenkinsproject.test;
+
+import android.test.AndroidTestCase;
+import android.webkit.ValueCallback;
+
+import com.cardinal.jenkinsproject.utils.AppHelper;
+
+public class UtilsTest extends AndroidTestCase {
+
+    public final void testUtilsDummyTestPass(){
+	int expectedValue = 1;
+	
+	AppHelper myAppHelper = new AppHelper();
+	int returnedValue = myAppHelper.getValue();
+	
+	//Check if the two values are equal
+	assertEquals(expectedValue, returnedValue);
+    }
+    public final void testUtilsDummyTestFail(){
+   	int expectedValue = 2;
+   	
+   	AppHelper myAppHelper = new AppHelper();
+   	int returnedValue = myAppHelper.getValue();
+   	
+   	//Check if the two values are equal
+   	assertEquals(expectedValue, returnedValue);
+       }
+    
+}
